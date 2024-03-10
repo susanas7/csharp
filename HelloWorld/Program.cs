59148175697f65c7ddd60dcff8b170e193b8a614
Susana Santamaria <susana.s7@hotmail.com>
2024-03-09 19:19:24 -0500

    Change Program to a class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld {
+    class Program {
+
+        static void Main() {
+            var name = "Jhon";
+            name = "1Jhon";
+
+            Console.WriteLine($"Hello World! {name}");
+            Console.WriteLine("");
+        }
+    }
+}
