6360865c7d3366c220dd3e9b97c06d92eccc931e
Susana Santamaria <susana.s7@hotmail.com>
2024-03-16 13:10:51 -0500

    Add excercise positive power

---
+++ positive_power/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Por favor, introduce un número positivo para elevarlo al cuadrado:");
+string input = Console.ReadLine();
+
+if (double.TryParse(input, out double number)) {
+    if (number > 0) {
+        double resultado = Math.Pow(number, 2);
+        Console.WriteLine($"El cuadrado de {number} es: {resultado}");
+        return;
+    }
+    
+    Console.WriteLine("El número ingresado no es positivo.");
+    return;
+}
+    
+Console.WriteLine("El dato es inválida. Por favor, introduce un número válido.");
