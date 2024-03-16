c64c2ef413def1e48ec39778ad53a48e2a1e8ec4
Susana Santamaria <susana.s7@hotmail.com>
2024-03-16 15:46:15 -0500

    Add excercise double or triple

---
+++ double_or_triple/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace DoubleOrTriple;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce el primer número:");
+        double numero1 = ObtenerNumeroDesdeConsola();
+
+        Console.WriteLine("Introduce el segundo número:");
+        double numero2 = ObtenerNumeroDesdeConsola();
+
+        double resultado = (numero1 > numero2) ? numero1 * 2 : numero2 * 3;
+
+        Console.WriteLine($"El resultado es: {resultado}");
+    }
+
+    static double ObtenerNumeroDesdeConsola()
+    {
+        double numero;
+        while (!double.TryParse(Console.ReadLine(), out numero))
+        {
+            Console.WriteLine("Entrada inválida. Por favor, introduce un número válido:");
+        }
+        return numero;
+    }
+}
