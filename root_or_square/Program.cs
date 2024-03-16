6d1dbced5c4b74bc0d99a13de52b8cf662c48c93
Susana Santamaria <susana.s7@hotmail.com>
2024-03-16 16:25:15 -0500

    Add excercise root or square

---
+++ root_or_square/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Introduce un número:");
+        
+        int numero;
+
+        while (!int.TryParse(Console.ReadLine(), out numero)) {
+            Console.WriteLine("Entrada inválida. Por favor, introduce un número válido:");
+        }
+
+        double resultado = (numero >= 0) ? Math.Sqrt(numero) : Math.Pow(numero, 2);
+
+        Console.WriteLine($"El resultado es: {resultado}");
+    }
+}
