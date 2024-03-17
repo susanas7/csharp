9a6be0556bb79fc28f784a4f24d46b84d4a32097
Susana Santamaria <susana.s7@hotmail.com>
2024-03-17 14:43:52 -0500

    Add excercise sum of evens

---
+++ sum_of_evens/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int sumaPares = 0;
+
+        for (int i = 1; i <= 50; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sumaPares += i;
+            }
+        }
+
+        Console.WriteLine(sumaPares);
+    }
+}
