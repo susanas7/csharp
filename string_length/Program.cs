04ae4f088e3f4f6cec57adeccc1ed7c4dd645cae
Susana Santamaria <susana.s7@hotmail.com>
2024-03-17 15:27:59 -0500

    Add exercise string legth

---
+++ string_length/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una palabra:");
+        string palabra = Console.ReadLine();
+
+        Console.WriteLine(palabra.Length);
+    }
+}
