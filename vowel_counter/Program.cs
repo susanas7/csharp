b4a53800815a72cef7fb8d7ab6cd756ab0b9bd4b
Susana Santamaria <susana.s7@hotmail.com>
2024-03-17 15:42:17 -0500

    Add exercise vowel counter

---
+++ vowel_counter/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Por favor, introduce una palabra:");
+        string palabra = Console.ReadLine();
+
+        int cantidadVocales = contarVocales(palabra);
+
+        Console.WriteLine(cantidadVocales);
+    }
+
+    static int contarVocales(string palabra)
+    {
+        int conteo = 0;
+        foreach (char letra in palabra)
+        {
+            if (esVocal(letra))
+            {
+                conteo++;
+            }
+        }
+        return conteo;
+    }
+
+    static bool esVocal(char letra)
+    {
+        letra = char.ToLower(letra);
+        return letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u';
+    }
+}
