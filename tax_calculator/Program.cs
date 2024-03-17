0843f6560e6559662ccd3c40093ddf27242ab59d
Susana Santamaria <susana.s7@hotmail.com>
2024-03-17 14:33:13 -0500

    Add excercise tax calculator

---
+++ tax_calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iintroduce tu salario anual:");
+        
+        double salarioAnual;
+        while (!double.TryParse(Console.ReadLine(), out salarioAnual) || salarioAnual < 0)
+        {
+            Console.WriteLine("Introduce un salario anual válido:");
+        }
+
+        if (salarioAnual > 12000)
+        {
+            double excedente = salarioAnual - 12000;
+            double impuesto = excedente * 0.15;
+
+            Console.WriteLine(impuesto);
+        }
+        else
+        {
+            Console.WriteLine("No debe impuestos");
+        }
+    }
+}
