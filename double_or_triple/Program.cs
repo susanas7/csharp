25698251a2c0f8259ebc0c1ec3bca2ca7c3e3d22
Susana Santamaria <susana.s7@hotmail.com>
2024-03-17 14:28:31 -0500

    Add double or tiple

--- double_or_triple/Program.cs
+++ double_or_triple/Program.cs
@@ -5,23 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Introduce el primer número:");
-        double numero1 = ObtenerNumeroDesdeConsola();
+        double numero1 = leerNumero();
 
         Console.WriteLine("Introduce el segundo número:");
-        double numero2 = ObtenerNumeroDesdeConsola();
+        double numero2 = leerNumero();
 
         double resultado = (numero1 > numero2) ? numero1 * 2 : numero2 * 3;
 
         Console.WriteLine($"El resultado es: {resultado}");
     }
 
-    static double ObtenerNumeroDesdeConsola()
+    static double leerNumero()
     {
         double numero;
-        while (!double.TryParse(Console.ReadLine(), out numero))
-        {
+        while (!double.TryParse(Console.ReadLine(), out numero)) {
             Console.WriteLine("Entrada inválida. Por favor, introduce un número válido:");
         }
+
         return numero;
     }
 }
